e27a9c0a01c93392315ca6c0a61b46c0815d7d57
FMLgamer <finnean2010@hotmail.com>
2021-10-27 20:59:09 +0200

    bli pblpo

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ from method.addPoly import addPoly  # ALOYS # CORRECT
 from method.addTable import addTable  # THOMAS
 from method.displayField import displayField # FINNEAN # CORRECT
 from method.displayPoly import displayPoly  # FINNEAN # CORRECT
-from method.divisionField import divisionField # FINNEAN
+from method.divisionField import divisionField # (FINNEAN) - ALOYS # CORRECT (maybe)
 from method.equalsField import equalsField # FINNEAN
 from method.equalsPolyMod import equalsPolyMod # FINNEAN
-from method.euclidPoly import euclidPoly  # FINNEAN 
+from method.euclidPoly import euclidPoly  # ALREADY SOME WORK DONE BY FINNEAN, PLEASE TAKE OVER 
 from method.findIrred import findIrred  # THOMAS
-from method.findPrim import findPrim
+from method.findPrim import findPrim 
 from method.inverseField import inverseField
 from method.irreducible import irreducible  # THOMAS
 from method.longDivPoly import longDivPoly  # FINNEAN # CORRECT
 from method.multiplyField import multiplyField # FINNEAN # CORRECT
 from method.multiplyPoly import multiplyPoly  # ALOYS # CORRECT
 from method.multTable import multTable
-from method.primitive import primitive
-from method.subtractField import subtractField
+from method.primitive import primitive 
+from method.subtractField import subtractField # FINNEAN # CORRECT
 from method.subtractPoly import subtractPoly  # ALOYS # CORRECT
 
 ### STUDENT PERSPECTIVE (example) ###
--- method/divisionField.py
+++ method/divisionField.py
@@ -2,11 +2,20 @@
 # Output:           STRING answer, POLY answer-poly
 # Functionality:    Compute the element f = a / b in F.
 #                   Output f as pretty print string and as POLY.
+from method.addPoly import addPoly
 from method.euclidPoly import longDiv
 from method.longDivPoly import longDivPoly
 from method.displayPoly import displayPoly
 
 
 def divisionField(mod, modPoly, a, b):
-    answer = longDivPoly(mod, longDiv(mod, a, b)[1], modPoly)[3]
-    return displayPoly(mod, answer), answer
+    if b == [0]:
+        return 'ERROR', []
+
+    q, r = longDivPoly(mod, a, b)[2:4]
+
+    while r != [0]:
+        a = addPoly(mod, a, modPoly)[1]
+        q, r = longDivPoly(mod, a, b)[2:4]
+        
+    return displayPoly(mod, q), q
