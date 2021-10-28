41f2013562159a227e18449464753c518a42a4f5
TMannens <thomasmannens@yahoo.nl>
2021-10-28 16:28:50 +0200

    fixedit

--- method/addTable.py
+++ method/addTable.py
@@ -4,28 +4,29 @@
 #                   once with elements as POLY’s.
 #                   In final format, the output is supposed to be a sequence of rows (see examples).
 
-from method.multiplyField import multiplyField
+from method.addField import addField
 from method.displayPoly import displayPoly
 
 def addTable(mod, modPoly):
     answer = [[0] * pow(mod, (len(modPoly)-1)) for i in range(pow(mod, (len(modPoly)-1)))]
-    answer2 = [[""] * pow(mod, (len(modPoly)-1)) for i in range(pow(mod, (len(modPoly)-1)))]
+    answer2 = [["0"] * pow(mod, (len(modPoly)-1)) for i in range(pow(mod, (len(modPoly)-1)))]
     axis = [0] * pow(mod, (len(modPoly)-1))
     for i in range(pow(mod, (len(modPoly)-1))):
         poly = [0] * (len(modPoly)-1)
         length = i
-        j =- 1
-        while length > mod:
-            j += 1
+        j = (len(modPoly)-1)
+        while length >= mod:
+            j -= 1
             poly[j] = length%mod
             length = int((length - poly[j]) / mod)
         if(not length == 0):
-            poly[j + 1] = length    
+            poly[j-1] = length    
         axis[i] = poly
     print(axis)
     for i in range(pow(mod, (len(modPoly)-1))):
         for j in range(pow(mod, (len(modPoly)-1))):
-            answer[i][j] = multiplyField(mod, modPoly, axis[i],axis[j])[1]
+            answer[i][j] = addField(mod, modPoly, axis[i],axis[j])[1]
+    print(answer)
     for i in range(pow(mod, (len(modPoly)-1))):
         for j in range(pow(mod, (len(modPoly)-1))):
             answer2[i][j] = displayPoly(mod, answer[i][j])
