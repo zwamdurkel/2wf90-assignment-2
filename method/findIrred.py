8d0ca898c304f28ff277eed14c26c3d64964c721
TMannens <thomasmannens@yahoo.nl>
2021-10-28 23:40:01 +0200

    intermediate pull

--- method/findIrred.py
+++ method/findIrred.py
@@ -6,11 +6,11 @@ from random import randint
 from method.irreducible import irreducible
 from method.displayPoly import displayPoly
 def findIrred(mod, deg):
-    a = [0] * (len(deg)+1)
+    a = [0] * (deg+1)
     for i in a:
-        a[i] = randint(0,mod-1)
+        a[i] = randint(0, mod-1)
     while not irreducible(mod, a):
         for i in a:
-            a[i] = randint(0,mod-1)
+            a[i] = randint(0, mod-1)
     b = displayPoly(mod, a)[1]
     return b, a
--- method/irreducible.py
+++ method/irreducible.py
@@ -4,17 +4,20 @@
 from method.euclidPoly import euclidPoly
 def irreducible(mod, f):
     t = 1
-    poly = [0] * mod
-    poly[0] =  mod-1
-    for i in range(mod-2):
-        poly[i+1] = 0
-    poly[mod-1] = 1
-    while euclidPoly(mod, f, poly)[5] == 1:
+    poly = [0] * (mod + 1)
+    poly[-1] =  0
+    poly[-2] =  mod-1
+    for i in range(mod-3):
+        poly[-(i+2)] = 0
+    poly[0] = 1
+    while euclidPoly(mod, f, poly)[5] == [1]:
         t += 1
-        poly[0] =  mod-1
-        for i in range(pow(mod,t)-2):
-            poly[i+1] = 0
-        poly[mod-1] = 1
+        poly = [0] * pow(mod + 1,t)
+        poly[-1] =  0
+        poly[-2] =  mod-1
+        for i in range(pow(mod + 1,t)-2):
+            poly[-(i+2)] = 0
+        poly[0] = 1
     if(t == len(f)-1):
         return True
     return False
